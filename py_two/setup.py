05108e18b2c1aa61ec158c0e8a22dd590622da33
Jeff Daily <jeff.daily@amd.com>
2021-07-06 20:45:05 +0000

    add py_one, py_two that both call runtime lib

---
+++ py_two/setup.py
@@ -0,0 +1,25 @@
+from glob import glob
+from setuptools import setup
+try:
+    from pybind11.setup_helpers import Pybind11Extension
+except ImportError:
+    from setuptools import Extension as Pybind11Extension
+
+ext_modules = [
+    Pybind11Extension(
+        "two_caller",
+        sources = sorted(glob("src/*.cpp")),  # Sort source files for reproducibility
+        libraries = ["racecar_runtime"],
+        library_dirs = ["/usr/local/lib"],
+        extra_compile_args = ["-std=c++11"]
+    ),
+]
+
+setup(
+    name="two",
+    version="1.0",
+    author="Nobody",
+    author_email="nobody@nobody.com",
+    ext_modules=ext_modules,
+    setup_requires=["pybind11"]
+)
